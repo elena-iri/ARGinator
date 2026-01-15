84033b0c447e2428e631e72ff71cdcf0c2c0772a
Claudio Emiliano Torres Cantu <claudio.torrescu@udlap.mx>
2026-01-15 11:32:27 +0100

    torch-lightning model implementation

--- src/arginator_protein_classifier/model.py
+++ src/arginator_protein_classifier/model.py
@@ -5,9 +5,33 @@ from pytorch_lightning import LightningModule
 from hydra.utils import instantiate
 from torchmetrics import Accuracy
 
-class Model(nn.Module):
-    def __init__(self, input_dim, output_dim, dropout_rate)->None:
+# class Model(nn.Module):
+#     def __init__(self, input_dim, output_dim, dropout_rate)->None:
+#         super().__init__()
+#         self.input_dim = input_dim,
+#         self.output_dim = output_dim,
+#         self.fc1 = nn.Linear(1024, 256)
+#         self.dropout=nn.Dropout(dropout_rate)
+#         self.fc2 = nn.Linear(256, 128)
+#         self.fc3 = nn.Linear(128, output_dim)
+#         self.relu = nn.ReLU()
+#         self.softmax = nn.Softmax(dim=1)
+
+#     def forward(self, x: torch.Tensor) -> torch.Tensor:
+#         x = self.fc1(x)
+#         x = self.relu(x)
+#         x = self.dropout(x)
+#         x = self.fc2(x)
+#         x = self.relu(x)
+#         x = self.dropout(x)
+#         x = self.fc3(x)
+#         return x
+    
+class Lightning_Model(LightningModule):
+    def __init__(self, input_dim, output_dim, dropout_rate, loss_fn, optimizer)->None:
         super().__init__()
+        self.save_hyperparameters() #save args for checkpointing
+        #architecture
         self.input_dim = input_dim,
         self.output_dim = output_dim,
         self.fc1 = nn.Linear(1024, 256)
@@ -16,6 +40,14 @@ class Model(nn.Module):
         self.fc3 = nn.Linear(128, output_dim)
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)
+        self.sigmoid = nn.Sigmoid()
+        # Loss & Optimizer are to change according to hydra
+        self.criterium = instantiate(loss_fn) 
+        self.optimizer = optimizer
+
+        #Metrics
+        self.train_acc = Accuracy(task="multiclass", num_classes=output_dim)
+        self.val_acc = Accuracy(task="multiclass", num_classes=output_dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.fc1(x)
@@ -27,28 +59,35 @@ class Model(nn.Module):
         x = self.fc3(x)
         return x
     
+    def training_step(self, batch, batch_idx):
+        data, target = batch
+        logits = self(data)
+        loss = self.criterium(logits, target)
+
+        # Logging
+        preds = torch.argmax(logits, dim=1)
+        self.train_acc(preds, logits)
+        self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("train_acc", self.train_acc, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss_fn(logits, y)
+        
+        preds = torch.argmax(logits, dim=1)
+        self.val_acc(preds, y)
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", self.val_acc, prog_bar=True)
+    
+    def configure_optimizers(self):
+        optimizer = instantiate(self.optimizer, params=self.parameters())
+        return optimizer
+    
 
 if __name__ == "__main__":
     x = torch.rand(1, 1024)
     mock = Model(x.shape[0], 2, 0.3)
     output = mock.forward(x)
     print(output.shape)
-
-# # Imagine this is your dataset (batch_size, feature_dim)
-# # Case A: 1024-dimensional embeddings
-# data_a = torch.randn(32, 1024) 
-
-# # Case B: 512-dimensional embeddings
-# data_b = torch.randn(32, 512)
-
-# # --- Dynamic Instantiation ---
-
-# # 1. Get the feature dimension from the data
-# # data.shape[1] gives the feature size (1024 or 512)
-# input_dim_a = data_a.shape[1] 
-
-# # 2. Pass it to the model
-# model = DynamicNet(input_size=input_dim_a, hidden_size=256, output_size=10)
-
-# print(f"Model created with input size: {model.layer1.in_features}")
-# # Output: Model created with input size: 1024
